a72318460cdf5bbc050282ab1d64c349d333d714
Akila Sachintha <akila.silva@rootcode.io>
2024-12-09 17:55:41 +0530

    remove enterprise resources

--- src/main/resources/enterprise/db/changelog/ddl-script/common-ddl-script-v1-add-column-user.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
-
--- changeset AkilaSachintha:common-ddl-script-v1-add-column-user
-ALTER TABLE `user`
-    ADD COLUMN `client_id` BIGINT NULL;
-
--- rollback DROP COLUMN `client_id` FROM `user`;
