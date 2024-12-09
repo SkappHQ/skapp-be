43811257deb2efe48d63d103613958af51003ecf
Akila Sachintha <akila.silva@rootcode.io>
2024-12-09 18:52:53 +0530

    fixed imports and remove unnecessary packages

--- src/test/java/com/skapp/community/peopleplanner/service/JobServiceImplUnitTest.java
+++ src/test/java/com/skapp/community/peopleplanner/service/JobServiceImplUnitTest.java
@@ -22,7 +22,6 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
