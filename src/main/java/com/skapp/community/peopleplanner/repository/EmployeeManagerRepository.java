cab9abc7f6d070b73e2dd68e9321f5d9fb029020
chathurikaAmarasinghe <chathurika.amarasinghe@rootcode.io>
2024-12-18 21:34:33 +0530

    fixing build failures

--- src/main/java/com/skapp/community/peopleplanner/repository/EmployeeManagerRepository.java
+++ src/main/java/com/skapp/community/peopleplanner/repository/EmployeeManagerRepository.java
@@ -3,8 +3,12 @@ package com.skapp.community.peopleplanner.repository;
 import com.skapp.community.leaveplanner.type.ManagerType;
 import com.skapp.community.peopleplanner.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeManagerRepository {
 
 	void deleteByEmployeeAndManagerType(Employee employee, ManagerType managerType);
+
+	List<Long> findManagerSupervisingEmployee(Long managerId);
 
 }
