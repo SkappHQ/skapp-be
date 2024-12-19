81f0e1b20921db03b53090453d473888be2e4ace
Akila Sachintha <akila.silva@rootcode.io>
2024-12-19 13:30:56 +0530

    fix: health version

--- src/main/java/com/skapp/community/common/controller/v1/HealthController.java
+++ src/main/java/com/skapp/community/common/controller/v1/HealthController.java
@@ -3,6 +3,7 @@ package com.skapp.community.common.controller.v1;
 import com.skapp.community.common.payload.response.HealthResponse;
 import com.skapp.community.common.type.HealthStatus;
 import com.skapp.community.common.util.DateTimeUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,12 @@ import java.time.Instant;
 @RestController
 public class HealthController {
 
+	@Value("${app.version}")
+	private String version;
+
 	@GetMapping("/health")
 	public ResponseEntity<HealthResponse> healthCheck() {
 		String formattedTimestamp = DateTimeUtils.DATE_TIME_FORMATTER.format(Instant.now());
-		String version = "1.0.1";
 
 		HealthResponse health = new HealthResponse(HealthStatus.HEALTHY, formattedTimestamp, version);
 		return ResponseEntity.ok(health);
