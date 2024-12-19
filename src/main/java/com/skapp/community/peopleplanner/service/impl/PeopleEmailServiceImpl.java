549b45bb0d193abae40e6d9305a75c5191a50b35
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2024-12-19 11:59:23 +0530

    fix: replace the email template selector

--- src/main/java/com/skapp/community/peopleplanner/service/impl/PeopleEmailServiceImpl.java
+++ src/main/java/com/skapp/community/peopleplanner/service/impl/PeopleEmailServiceImpl.java
@@ -7,6 +7,7 @@ import com.skapp.community.common.repository.UserDao;
 import com.skapp.community.common.service.EmailService;
 import com.skapp.community.common.service.EncryptionDecryptionService;
 import com.skapp.community.common.type.EmailBodyTemplates;
+import com.skapp.community.common.type.LoginMethod;
 import com.skapp.community.leaveplanner.model.LeaveRequest;
 import com.skapp.community.peopleplanner.model.EmployeeManager;
 import com.skapp.community.peopleplanner.model.EmployeeRole;
@@ -62,8 +63,15 @@ public class PeopleEmailServiceImpl implements PeopleEmailService {
 		emailDynamicFields.setWorkEmail(user.getEmail());
 		emailDynamicFields.setTemporaryPassword(encryptionDecryptionService.decrypt(user.getTempPassword(), secretKey));
 
-		emailService.sendEmail(EmailBodyTemplates.PEOPLE_MODULE_USER_INVITATION_V1, emailDynamicFields,
-				emailDynamicFields.getWorkEmail());
+		if (user.getLoginMethod() == LoginMethod.GOOGLE) {
+			emailService.sendEmail(EmailBodyTemplates.PEOPLE_MODULE_USER_INVITATION_SSO, emailDynamicFields,
+					emailDynamicFields.getWorkEmail());
+		}
+		else if (user.getLoginMethod() == LoginMethod.CREDENTIALS) {
+			emailService.sendEmail(EmailBodyTemplates.PEOPLE_MODULE_USER_INVITATION_V1, emailDynamicFields,
+					emailDynamicFields.getWorkEmail());
+		}
+
 	}
 
 	@Override
