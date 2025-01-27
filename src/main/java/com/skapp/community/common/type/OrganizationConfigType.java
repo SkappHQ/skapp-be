9761d4032e361973541834bec327ebad7381b7f6
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-01-27 16:17:17 +0530

    feat(common): google calendar config for organization (#134)

--- src/main/java/com/skapp/community/common/type/OrganizationConfigType.java
+++ src/main/java/com/skapp/community/common/type/OrganizationConfigType.java
@@ -2,6 +2,6 @@ package com.skapp.community.common.type;
 
 public enum OrganizationConfigType {
 
-	LEAVE_CYCLE, HOURS_PER_DAY, EMAIL_CONFIGS
+	LEAVE_CYCLE, HOURS_PER_DAY, EMAIL_CONFIGS, CALENDAR_CONFIGS
 
 }
