3c56d66e38daa8b998a6c67effc785d29f84857c
Akila Sachintha <akila.silva@rootcode.io>
2025-02-14 15:20:28 +0530

    Feat: #372 - build failure fixes

--- src/main/java/com/skapp/community/common/mapper/CommonMapper.java
+++ src/main/java/com/skapp/community/common/mapper/CommonMapper.java
@@ -9,10 +9,7 @@ import com.skapp.community.peopleplanner.model.Employee;
 import com.skapp.community.peopleplanner.model.Holiday;
 import com.skapp.community.peopleplanner.payload.response.EmployeeTeamResponseDto;
 import com.skapp.community.peopleplanner.payload.response.HolidayResponseDto;
-import com.skapp.enterprise.common.model.master.Tenant;
-import com.skapp.enterprise.common.payload.request.CreateSubscriptionResponseDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommonMapper {
@@ -26,9 +23,5 @@ public interface CommonMapper {
 	OrganizationResponseDto organizationToOrganizationResponseDto(Organization organization);
 
 	HolidayResponseDto holidayToHolidayResponseDto(Holiday holiday);
-
-	@Mapping(target = "subscriptionId", source = "stripeSubscription.subscriptionId")
-	@Mapping(target = "customerId", source = "stripeSubscription.customerId")
-	CreateSubscriptionResponseDto tenantToCreateSubscriptionResponseDto(Tenant tenant);
 
 }
