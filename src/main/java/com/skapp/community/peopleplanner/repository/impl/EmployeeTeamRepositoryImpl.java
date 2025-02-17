cc4ca880c6163f7ff78edbb3e36eb767b2127dba
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-02-17 11:28:56 +0530

    fix: clockin summary filter issue

--- src/main/java/com/skapp/community/peopleplanner/repository/impl/EmployeeTeamRepositoryImpl.java
+++ src/main/java/com/skapp/community/peopleplanner/repository/impl/EmployeeTeamRepositoryImpl.java
@@ -181,8 +181,7 @@ public class EmployeeTeamRepositoryImpl implements EmployeeTeamRepository {
 					criteriaBuilder.or(isAdminPredicate, employeeRoot.get(Employee_.employeeId).in(employeesSubquery)));
 		}
 		else {
-			predicates.add(criteriaBuilder.or(isAdminPredicate,
-					employeeTeamJoin.get(EmployeeTeam_.team).get(Team_.teamId).in(teams)));
+			predicates.add(employeeTeamJoin.get(EmployeeTeam_.team).get(Team_.teamId).in(teams));
 		}
 
 		if (clockInType.contains(ClockInType.NOT_CLOCKED_INS) && !clockInType.contains(ClockInType.LATE_CLOCK_INS)) {
