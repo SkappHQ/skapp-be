5b8e55c4fbb0e8f392a115f03423ede1c7903215
HarinduMalinka <harindu.desilva@rootcode.io>
2025-02-23 20:26:34 +0530

    optimizations and date time convers

--- src/main/java/com/skapp/community/common/type/EmailBodyTemplates.java
+++ src/main/java/com/skapp/community/common/type/EmailBodyTemplates.java
@@ -112,11 +112,13 @@ public enum EmailBodyTemplates {
 
 	// Payment Templates for stripe
 	PAYMENT_STRIPE_PAYMENT_WAS_UNSUCCESSFUL_TRIAL_END_DATE("payment-stripe-payment-was-unsuccessful-trial-end-date"),
-	PAYMENT_STRIPE_PAYMENT_WAS_UNSUCCESSFUL_AFTER_3DAYS_AND_5DAYS("payment-stripe-payment-was-unsuccessful-after-3days-and-5days"),
+	PAYMENT_STRIPE_PAYMENT_WAS_UNSUCCESSFUL_AFTER_3DAYS_AND_5DAYS(
+			"payment-stripe-payment-was-unsuccessful-after-3days-and-5days"),
 	PAYMENT_STRIPE_PAYMENT_WAS_UNSUCCESSFUL_AFTER_7DAYS("payment-stripe-payment-was-unsuccessful-after-7days"),
 	PAYMENT_STRIPE_WELCOME_TO_SKAPP_PRO_FREE_TRIAL("payment-stripe-welcome-to-skapp-pro-free-trial"),
 	PAYMENT_STRIPE_FREE_TRIAL_EXPIRES_IN_3DAYS("payment-stripe-free-trial-expires-in-3days"),
-	PAYMENT_STRIPE_CONGRATULATIONS_ON_UPGRADING_TO_SKAPP_PRO("payment-stripe-congratulations-on-upgrading-to-skapp-pro");
+	PAYMENT_STRIPE_CONGRATULATIONS_ON_UPGRADING_TO_SKAPP_PRO(
+			"payment-stripe-congratulations-on-upgrading-to-skapp-pro");
 
 	private final String templateId;
 
