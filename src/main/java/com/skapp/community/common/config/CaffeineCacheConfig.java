a771aa8154994b4be7223b3b2d4e59b1f6e5f8fc
Akila Sachintha <akila.silva@rootcode.io>
2025-02-26 09:08:36 +0530

    feat: conditional compiling

--- src/main/java/com/skapp/community/common/config/CaffeineCacheConfig.java
+++ src/main/java/com/skapp/community/common/config/CaffeineCacheConfig.java
@@ -2,12 +2,14 @@ package com.skapp.community.common.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@ConditionalOnMissingBean(name = "RedisConfig")
 public class CaffeineCacheConfig {
 
 	@Bean
