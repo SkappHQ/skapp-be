b432ac3f7b8d7b1b1353430ba29287c6649cc290
Akila Sachintha <akila.silva@rootcode.io>
2025-02-27 21:38:46 +0530

    feat: update subscription flow

--- src/main/java/com/skapp/community/common/type/SystemVersionTypes.java
+++ src/main/java/com/skapp/community/common/type/SystemVersionTypes.java
@@ -2,6 +2,6 @@ package com.skapp.community.common.type;
 
 public enum SystemVersionTypes {
 
-	INITIAL_VERSION, TIER_CHANGE_FROM_FREE_TO_PRO, MODULE_CHANGE
+	INITIAL_VERSION, TIER_CHANGE_FROM_FREE_TO_PRO, TIER_CHANGE_FROM_PRO_TO_FREE, MODULE_CHANGE
 
 }
