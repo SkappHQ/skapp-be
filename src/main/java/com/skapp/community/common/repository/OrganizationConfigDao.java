a645815c109c6f0d127baacc276b573f0c865b2f
Akila Sachintha <akila.silva@rootcode.io>
2025-03-06 18:17:01 +0530

    format changes

--- src/main/java/com/skapp/community/common/repository/OrganizationConfigDao.java
+++ src/main/java/com/skapp/community/common/repository/OrganizationConfigDao.java
@@ -1,7 +1,6 @@
 package com.skapp.community.common.repository;
 
 import com.skapp.community.common.model.OrganizationConfig;
-import com.skapp.community.common.type.OrganizationConfigType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
