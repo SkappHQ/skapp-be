62831c067122c504d917c5b9e37bf87dab32add3
yuhanga-rtc <yuhanga.induwara@rootcode.io>
2025-04-18 08:58:38 +0530

    fix: removed EP related cache keys from community

--- src/main/java/com/skapp/community/common/type/CacheKeys.java
+++ src/main/java/com/skapp/community/common/type/CacheKeys.java
@@ -7,11 +7,10 @@ import java.util.concurrent.TimeUnit;
 
 @Getter
 @RequiredArgsConstructor
-public enum CacheKeys {
+public enum CacheKeys implements CacheKey {
 
 	SYSTEM_VERSION_CACHE_KEY("system_version_cache:latest_version", 7, TimeUnit.DAYS),
-	USER_VERSION_CACHE_KEY("user_version_cache:%s:latest_version", 7, TimeUnit.DAYS), // userId
-	UUID_CACHE_KEY("uuid_cache:%s", 5, TimeUnit.MINUTES);
+	USER_VERSION_CACHE_KEY("user_version_cache:%s:latest_version", 7, TimeUnit.DAYS); // userId
 
 	private final String key;
 
